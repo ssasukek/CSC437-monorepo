7ed1827b0d90f657c32472d5ee2d884be7f92d51
sasukek <kevinlu3234@gmail.com>
2025-04-29 12:30:24 +0000

    lab7 done

--- packages/proto/public/darkmode.js
+++ packages/proto/public/darkmode.js
@@ -1,15 +1,8 @@
-const label = document.getElementById("darkmode-toggle");
-label.onchange = (evt) => {
-    evt.stopProgration();
-    const detail = { enabled: evt.target.checked };
-    const darkEvt = new CustomEvent("darkmode:toggle", { detail });
-    label.dispatchEvent(darkEvt);
-};
-
-document.body.addEventListener("darkmode:toggle", (evt) => {
-    if (evt.detail.enabled) {
-    document.body.classList.add("dark-mode");
-    } else {
-    document.body.classList.remove("dark-mode");
-    }
-});
+const checkbox = document.querySelector('.dark-checkbox');
+if (!checkbox) {
+  console.error('darkmode.js: .dark-checkbox not found');
+} else {
+  checkbox.addEventListener('change', e => {
+    document.body.classList.toggle('dark-mode', e.target.checked);
+  });
+}
