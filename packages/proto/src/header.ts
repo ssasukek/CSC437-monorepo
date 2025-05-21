017d0a8a8d3fa90f861fcf89d09d4cce8eda3105
sasukek <kevinlu3234@gmail.com>
2025-05-21 06:41:25 +0000

    trade-card fetch

---
+++ packages/proto/src/header.ts
@@ -0,0 +1,42 @@
+import { LitElement, html } from "lit";
+import { state } from "lit/decorators.js";
+import { Observer, Events, Auth } from "@calpoly/mustang";
+
+export class HeaderElement extends LitElement {
+  _authObserver = new Observer<Auth.Model>(this, "blazing:auth");
+  @state() loggedIn = false;
+  @state() userid?: string;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._authObserver.observe(auth => {
+      const u = auth.user;
+      this.loggedIn = !!u?.authenticated;
+      this.userid   = u?.authenticated ? u.username : undefined;
+    });
+  }
+
+  renderSignOutButton() {
+    return html`
+      <button
+        @click=${(e: UIEvent) =>
+          Events.relay(e, "auth:message", ["auth/signout"])}
+      >
+        Sign Out
+      </button>
+    `;
+  } 
+  renderSignInButton() {
+    return html`
+      <a href="/login.html">
+        Sign In…
+      </a>
+    `;
+  }
+  render() {
+    return html`
+      <slot name="actuator">Hello, ${this.userid||"traveler"}</slot>
+      ${this.loggedIn ? this.renderSignOutButton() : this.renderSignInButton()}
+    `;
+  }
+}
